461a104dc9fd9e589674aed959c178b87d1647b5
Kelechi Okwuriki <kelechi@bento.africa>
2021-10-06 17:46:06 +0100

    updated read me

---
+++ README.md
@@ -0,0 +1,16 @@
+
+## Instructions
+1. Navigate to project directory in terminal and run cp .env.example .env
+2. Create database and update .env with database details
+   
+## Install dependecies
+While in project directory in terminal, run
+
+"composer install"
+
+## Run subscriber server
+php artisan serve
+
+## IMPORTANT!!!
+Use served url and port when creating a subscription on Publisher service.
+  
---
+++ app/Http/Controllers/Api/SubscriberApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class SubscriberApiController extends Controller
+{
+    protected $subscriberService;
+
+    public function processMessage(Request $data)
+    {
+        if (count($data->all()) > 0) {
+            return response()->json([
+                'message' => 'successful',
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => 'Bad request',
+        ], 400);
+    }
+}
